1d96d9500bff951c5345f1b1b968de04574bfa3d
Aqua3k <c3ryumon@gmail.com>
2022-01-26 22:51:14 +0900

    リファクタリング実行

--- DebugLib.py
+++ DebugLib.py
@@ -14,40 +14,30 @@ def ExacProg():
     #プログラムの実行(名前が変わったなら変える)
     t_start = time.time()
     errMessage = "NaN"
+    name = os.path.basename(fl.GetFileName())
     try:
         import main
         main.print = DebugPrint
         main.input = DebugInput
         main.main()
-    except Exception as e:
-        name = fl.GetFileName().split("\\")[-1]
+    except Exception as e:        
         print("error in ", name)
         errMessage = str(e)
     t_end = time.time()
 
-    length = len(statisticsInfoArray)
-    name = fl.GetFileName().split("\\")[-1]
-    t = t_end - t_start
-    csvArr = [name, str(t)]
+    csvArr = [name, str(t_end - t_start)]
     for i, name in enumerate(statisticsInfoArray):
-        try:
-            cont = getattr(main, name)
-        except:
-            cont = errMessage
-        if i == length - 1:
-            SetScore(cont)
+        try:    cont = getattr(main, name)
+        except: cont = errMessage
+        if i == len(statisticsInfoArray) - 1:
+            fl.SetScoreSub(cont)
             print(cont)
         csvArr.append(cont)
     
-    AddCSVFile(csvArr)
+    sl.AddCSVFileSub(csvArr)
 
     #Pythonは自動でimportガードがついてるので一度モジュールを削除する
     if 'main' in sys.modules: del sys.modules["main"]
-
-####################################
-#statisticsの管理
-def AddCSVFile(ar):
-    sl.AddCSVFileSub(ar)
 
 ####################################
 #summaryファイル, csv, mainファイルをコピーして保存する
@@ -88,64 +78,41 @@ def InitLogFile():
     f.close()
 
 def GetAllFileName():
-    global inputFilePath
     return glob.glob(os.path.join(inputFilePath, "*"))
 
 def GetLogFileName():
-    fileName =  fl.GetFileName().split("\\")[-1]
-    logFileName = "log_" + fileName
-    return logFileName
+    return "log_" + os.path.basename(fl.GetFileName())
 
 ####################################
 #scoreの管理
-def SetScore(score):
-    fl.SetScoreSub(score)
-
 def WriteScore():
-
     global scoreFilePath, scoreFileName
     scores = fl.GetScore()
-    length = len(scores)
-    if length == 0: return
-    scoreSum = 0
-    scoreMin = float("Inf")
-    scoreMinFileName = 0
-    scoreMax = -float("Inf")
-    scoreMaxFileName = 0
-    for i, [fileName, scoreStr] in enumerate(scores):
-        score = 0
-        try:
-            score = int(scoreStr)
-        except:
-            pass
-        fileName = fileName.split("\\")[-1]
-        if score < scoreMin:
-            scoreMin = score
-            scoreMinFileName = fileName
-        if scoreMax < score:
-            scoreMax = score
-            scoreMaxFileName = fileName
-        scoreSum += score
-
+    fileNameList, scoresList = [], []
+    for i, [filePath, scoreStr] in enumerate(scores):
+        fileName = os.path.basename(filePath)
+        try:    score = int(scoreStr)
+        except: score = 0
+        fileNameList.append(fileName)
+        scoresList.append(score)
+    
     f = open(os.path.join(scoreFilePath, scoreFileName), 'w')
-    f.write(str(length) + " files inputs" + "\n")
-    f.write("score average is " + str(scoreSum/length) + "\n")
-    f.write("max score is " + str(scoreMax) + ", filename is " + scoreMaxFileName + "\n")
-    f.write("min score is " + str(scoreMin) + ", filename is " + scoreMinFileName + "\n")
+    f.write(str(len(scores)) + " files inputs" + "\n")
+    f.write("score average is " + str(sum(scoresList)/len(scores)) + "\n")
+    f.write("max score is " + str(max(scoresList)) + ", filename is " + fileNameList[scoresList.index(max(scoresList))] + "\n")
+    f.write("min score is " + str(min(scoresList)) + ", filename is " + fileNameList[scoresList.index(min(scoresList))] + "\n")
     f.write("\n")
     for fileName, score in scores:
-        fileName = fileName.split("\\")[-1]
-        f.write(fileName + " " + str(score) + "\n")
+        f.write(os.path.basename(fileName) + " " + str(score) + "\n")
     f.close()
 
 ####################################
 #main
 
 def main():
-    fileLis = GetAllFileName()
     ar = ["FileName", "Time"] + statisticsInfoArray
     sl.InitCSV(ar)
-    for filename in fileLis:
+    for filename in GetAllFileName():
         fl.SetFileName(filename)
         fl.SetFileContents()
         
