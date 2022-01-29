6cd98d4088dc3fc3c174e88dfe48b294950141d1
Aqua3k <c3ryumon@gmail.com>
2022-01-29 15:43:04 +0900

    不具合修正

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-def Input():
-    n,m,k,r = map(int, input().split())
-    d = [[None]] + [list(map(int, input().split())) for i in range(n)]
-    uv = [[None]] +[list(map(int, input().split())) for i in range(r)]
-    return n,m,k,r,d,uv
-
-score = 0
-k,r = 0,0
 def main():
-    global score,k,r
-    n,m,k,r,d,uv = Input()
-
-    score = n+m+k+r
-    print(score)
-
+    pass
 if __name__ == '__main__':
     main()
