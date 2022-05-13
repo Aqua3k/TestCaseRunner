979c890bfb4e79d668a174919b439c0028753665
Aqua3k <c3ryumon@gmail.com>
2022-05-13 22:45:19 +0900

    docstringをgoogleスタイルに修正 未使用の処理を削除

--- DebugMain.py
+++ DebugMain.py
@@ -4,32 +4,24 @@ import time
 import os
 import shutil
 import subprocess
-from typing import Any
 
 from mysrc.settings import *
 from mysrc.MyLib import *
 from mysrc.HTMLtemplate import *
 from mysrc.Output import InitAll, MakeAllResult
 
-####################################
-
 inFile = "in.txt"
 outFile = "out.txt"
-
-def DebugPrint(*arg: Any, **keys: Any) -> None:
-    """Debug用の出力"""
-    f = open(os.path.join(resultFilePath, os.path.basename(File.GetFileName())), 'a')
-    print(*arg, **keys, file=f)
-    f.close()
-
-def DebugInput() -> str:
-    """Debug用の入力"""
-    return str(File.GetFileContentsLine())
-
-####################################
+nextInputFileName = None #次に実行するケースの名前
 
 def GetScoreFromStandardOutput(string: str) -> int:
-    """標準出力から得点を取り出す"""
+    """標準出力から得点を取り出す
+    
+    Args:
+        string(str): 得点を取り出す元の文字列
+    Returns:
+        int: 得点
+    """
     u = string.lower()
     if "score" in u:
         idx = u.index("score")
@@ -48,15 +40,19 @@ def GetScoreFromStandardOutput(string: str) -> int:
     return ret
 
 def EndProcess():
+    """プログラム終了の処理"""
     time.sleep(3) # コマンド実行の関係で少し待機させる
     if os.path.isfile(inFile): os.remove(inFile)
     if os.path.isfile(outFile): os.remove(outFile)
 
-####################################
 def ExacProg() -> ResultInfo:
-    """プログラムを実行して結果を返す"""
+    """プログラムを実行して結果を返す
+    
+    Returns:
+        ResultInfo: 実行結果の情報
+    """
     t_start = time.time()
-    name = os.path.basename(File.GetFileName())
+    name = os.path.basename(nextInputFileName)
     
     score, errStatus, stdOut = ExacCommand(name)
 
@@ -77,6 +73,14 @@ def ExacProg() -> ResultInfo:
     return ResultInfo(name, score, t_end-t_start, errStatus, stdOut, lis)
 
 def ExacCommand(name: str):
+    """プログラムを実行する
+    
+    Args:
+        name(str): 実行対象の入力ファイルの名前
+    
+    Returns:
+        int, int, str: 得点, 結果のステータス, 標準出力
+    """
     timeLimit = 2
     errStatus = ResultInfo.AC
     score = ""
@@ -108,29 +112,20 @@ def ExacCommand(name: str):
         if errStatus == ResultInfo.RE:
             score = "RE"
             msg = "RE "
-            errMsg = r.stdout
         else:
             score = "TLE"
             msg = "TLE"
-            errMsg = "Time Limit Exceeded."
         print(msg + " in ", name)
-        DebugPrint("------------------------------")
-        DebugPrint(errMsg)
     
     return score, errStatus, stdOut
 
-####################################
-#main
-
-File = None
 def main() -> None:
-    global File
-    File = FileControl()
+    """main処理"""
+    global nextInputFileName
     resultAll = []
     InitAll()
     for filename in glob.glob(os.path.join(inputFilePath, "*")):
-        File.SetFileName(filename)
-        File.SetFileContents()
+        nextInputFileName = filename
         result = ExacProg()
         resultAll.append(result)
     MakeAllResult(resultAll)
