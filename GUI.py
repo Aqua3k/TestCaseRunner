fe7c85e5a10cfd7b3eeafb04d89c10e3e9515ed1
Aqau3k <c3ryumon@gmail.com>
2022-12-27 15:23:18 +0900

    ログを別windowに

--- GUI.py
+++ GUI.py
@@ -11,7 +11,8 @@ def get_layout():
     layout = [
                 [sg.Button('UPDATE')],
                 [sg.Button('RUN SCRIPT')],
-                [sg.Button('EDIT SETTINGS')]
+                [sg.Button('EDIT SETTINGS')],
+                [sg.Button('SHOW LOGS')]
     ]
     for folder in glob.glob(r"log\*"):
         layout.append([sg.Text(folder), sg.Button('VIEW RESULT', key=("view", folder)), sg.Button('DELETE', key=("delete", folder))])
@@ -50,6 +51,79 @@ def update_window():
     window.close()
     window = sg.Window('Test Case Runner', get_layout())
 
+meta_datas = [
+    "FILE NUMBER",
+    "AVERAGE SCORE",
+    "MAX SCORE",
+    "MIN SCORE",
+]
+meta_data_headers = [
+    "DATE",
+    "INPUTS",
+    "AVE.",
+    "MAX",
+    "MIN",
+]
+log_list = None
+def get_results():
+    global log_list
+
+    def parse_date(folder):
+        """ファイルの名前から'mm/dd xx:yy'の形式に変換する"""
+        filename = os.path.split(folder)[1]
+        return f"{filename[4:6]}/{filename[6:8]} {filename[8:10]}:{filename[10:12]}"
+    
+    def get_digits(string):
+        ret = ""
+        for s in string:
+            if "0" <= s <= "9" or s in ".-":
+                ret += s
+        try:
+            return int(ret)
+        except:
+            return 0
+
+    def get_meta_data(folder):
+        html_path = glob.glob(folder + r"\*.html")[0]
+        meta_data = []
+        date = parse_date(folder)
+        meta_data.append(date)
+        with open(html_path) as f:
+            lines = f.readlines()
+            for header in meta_datas:
+                for i,line in enumerate(lines):
+                    if header in line:
+                        break
+                meta_data.append(get_digits(lines[i+1]))
+        return meta_data
+
+    ret = []
+    log_list = glob.glob(r"log\*")
+    for folder in log_list:
+        meta_data = get_meta_data(folder)
+        ret.append(meta_data)
+    return ret
+
+def get_log_viewer_window_layout():
+    results = get_results()
+    layout = [
+        [sg.Button('ERASE'), sg.Button('SHOW DETAILS')],
+        [sg.Table(
+            results,
+            headings=meta_data_headers,
+            auto_size_columns=False,
+            #col_widths=[15, 5, 10, 10, 10],
+            justification='left',
+            text_color='#000000',
+            background_color='#cccccc',
+            alternating_row_color='#ffffff',
+            header_text_color='#0000ff',
+            header_background_color='#cccccc',
+            size=(40, 20),
+            key="TABLE")]
+        ]
+    return layout
+
 window = None
 def main():
     global window
@@ -70,7 +144,7 @@ def main():
             run_script()
             update_window()
         elif event == 'EDIT SETTINGS':
-            config_window = sg.Window('Test Case Runner', get_config_window_layout())
+            config_window = sg.Window('Setting Editor', get_config_window_layout())
             while True:
                 event, values = config_window.read()
                 if event == "__TIMEOUT__":
@@ -79,6 +153,31 @@ def main():
                     save_setting(values)
                 elif event == sg.WIN_CLOSED:
                     break
+        elif event == 'SHOW LOGS':
+            log_viewer = sg.Window('Log Viewer', get_log_viewer_window_layout())
+            while True:
+                event, values = log_viewer.read()
+                if event == "__TIMEOUT__":
+                    continue
+                elif event == sg.WIN_CLOSED:
+                    break
+                elif event == "ERASE":
+                    if len(values["TABLE"]) == 0:
+                        continue
+                    idx = values["TABLE"][0]
+                    filder = log_list[idx]
+                    shutil.rmtree(filder)
+                    log_viewer.close()
+                    log_viewer = sg.Window('Log Viewer', get_log_viewer_window_layout())
+                elif event == "SHOW DETAILS":
+                    if len(values["TABLE"]) == 0:
+                        continue
+                    idx = values["TABLE"][0]
+                    path = os.path.join(log_list[idx], "*.html")
+                    files = glob.glob(path)
+                    assert len(files) == 1
+                    html_path = files[0]
+                    subprocess.run(["start", html_path], shell=True)
         else:
             key, folder = event
             if key == "view":
