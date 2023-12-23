1933a5a69e7bfb74b3b62284c442289b04ab6ac0
Aqau3k <c3ryumon@gmail.com>
2023-12-23 15:32:45 +0900

    HTMLが作れるようにした

--- debug_main.py
+++ debug_main.py
@@ -46,13 +46,14 @@ class ResultStatus(Enum):
     AC = auto()
     RE = auto()
     TLE = auto()
+    RUNNER_ERROR = auto()
 
 @dataclass
 class TestCaseResult:
     error_status: ResultStatus
     stdout: str
     stderr: str
-    result: Dict[str, Union[int, float]]
+    attribute: Dict[str, Union[int, float]]
 
 class TestCaseRunner:
     def __init__(self, settings: Settings, handler: Callable[[str, str], Any]):
@@ -94,6 +95,123 @@ class TestCase:
             f.write(self.test_result.stderr)
         return self
 
+from mysrc.html_templates import *
+class HtmlMaker:
+    @dataclass
+    class Column:
+        title: str
+        getter: Callable[[int], str]
+
+    def __init__(self, results: List[TestCase]):
+        self.results = results
+    
+    def sortup_attributes(self):
+        attributes = set()
+        for result in self.results:
+            test_result = result.test_result
+            for attribute in test_result.attribute.keys():
+                attributes.add(attribute)
+        return list(attributes)
+    
+    def get_in(self, attribute, row: int):
+        return table_cell.format(text=html_link_str.format(path=self.results[row].input_file, string="+"))
+    def get_stdout(self, attribute, row: int):
+        return table_cell.format(text=html_link_str.format(path=self.results[row].stdout_file, string="+"))
+    def get_testcase_name(self, attribute, row: int):
+        return table_cell.format(text=self.results[row].test_case_name)
+    def get_stderr(self, attribute, row: int):
+        return table_cell.format(text=html_link_str.format(path=self.results[row].stderr_file, string="+"))
+    status_texts = {
+        ResultStatus.AC: ("AC", "lime"),
+        ResultStatus.RE: ("RE", "gold"),
+        ResultStatus.TLE: ("TLE", "gold"),
+        ResultStatus.RUNNER_ERROR: ("IE", "red"),
+    }
+    def get_status(self, attribute, row: int):
+        status = self.results[row].test_result.error_status
+        if status not in self.status_texts:
+            status = ResultStatus.RUNNER_ERROR
+        text, color = self.status_texts[status]
+        return table_colored_cell.format(color=color, text=text)
+    def get_other(self, _, attribute: str, row: int):
+        attributes = self.results[row].test_result.attribute
+        if attribute not in attributes:
+            value = "---"
+        else:
+            value = attributes[attribute]
+            if type(value) is float:
+                value = round(value, 3)
+        return table_cell.format(text=str(value))
+
+    columns = [
+        Column("in", get_in),
+        Column("out", get_stdout),
+        Column("stdout", get_stderr),
+        Column("testcase", get_testcase_name),
+        Column("status", get_status),
+    ]
+    def make(self):
+        attributes = self.sortup_attributes()
+        for attribute in attributes:
+            self.columns.append(self.Column(attribute, self.get_other))
+        
+        table_body = []
+        # テーブルのタイトルを作る
+        table = ""
+        for column in self.columns:
+            table += f'<th>{column.title}</th>'
+        table_body.append(table_line.format(text=table))
+
+        # テーブル本体を作る
+        for row in range(len(self.results)):
+            body = ""
+            for column in self.columns:
+                body += column.getter(self, column.title, row)
+            table_body.append(table_line.format(text=body))
+        
+        table_all = "<h2>Table</h2>"
+        table_all += table_heading.format(body="\n".join(table_body))
+        now = datetime.datetime.now()
+        body = f'<h6>Creation date and time: {now.strftime("%Y/%m/%d %H:%M:%S")}</h6>'
+        
+        body += "<h2>Summary</h2>"
+        body += self.make_summary()
+        body += table_all
+
+        result_file_name = "result.html"
+        with open(result_file_name ,'w', encoding='utf-8', newline='\n') as html:
+            text = html_text.format(body=body, title="Result")
+            text = self.insert_text_into_html_head("<body>", text, css_link1)
+            text = self.insert_text_into_html_head("<body>", text, css_link2)
+            text = self.insert_text_into_html_head("<body>", text, script_link)
+            html.writelines(text)
+
+    def make_summary(self) -> str:
+        """サマリ情報を作る"""
+        return ""
+        file_name_list, scores_list = [], []
+        for result in self.result_all:
+            file_name_list.append(os.path.basename(result.name))
+            scores_list.append(0 if result.score == "None" or result.err_stat != ResultInfo.AC else int(result.score))
+
+        string = []
+        string.append("Input file number: " + str(len(self.result_all)))
+        string.append("Average Score: " + str(sum(scores_list)/len(self.result_all)))
+        string.append("")
+        string.append("Max Score: " + str(max(scores_list)))
+        string.append("FileName: " + file_name_list[scores_list.index(max(scores_list))])
+        string.append("")
+        string.append("Minimum Score: " + str(min(scores_list)))
+        string.append("FileName: " + file_name_list[scores_list.index(min(scores_list))])
+        string.append("")
+        return "<br>\n".join(string)
+    
+    def insert_text_into_html_head(self, tag: str, html_str: str, text: str) -> str:
+        """HTMLの文字列のtagの中に別の文字列を挿入する"""
+        html_str_list = html_str.split("\n")
+        html_str_list.insert(html_str_list.index(tag) + 1, text)
+        return "\n".join(html_str_list)
+
 def test_handler(input, output):
     cmd = f"cargo run --release --bin tester python main.py < {input}"
     start_time = time.time()
@@ -116,7 +234,9 @@ def main():
     setting = get_setting()
     runner = TestCaseRunner(setting, test_handler)
     results = runner.run()
-    make_results(results)
+    html_maker = HtmlMaker(results)
+    html_maker.make()
+    #make_results(results)
 
 if __name__ == "__main__":
     main()
