7fb67e1d68b788d9e81de6832eeae970c1fcf7c5
aqua <c3ryumon@gmail.com>
2024-01-05 18:18:36 +0900

    log viewerを作成

--- app.py
+++ app.py
@@ -6,17 +6,20 @@ import json
 from flask import Flask
 from flask_cors import CORS
 import pandas as pd
+from jinja2 import Environment, FileSystemLoader
 
 from testcase_runner.testcase_runner import ResultStatus
 
 log_dir = "log"
 log_file = "result.json"
+html_path = "result.html"
 
 class Log():
     def __init__(self, path):
         with open(path, 'r') as file:
             json_data = json.load(file)
         tmp = os.path.split(path)[0]
+        self.html_path = os.path.join(tmp, html_path)
         self.foleder_name = os.path.basename(tmp)
         contents = json_data["contents"]
         self.testcase_num = json_data["testcase_num"]
@@ -30,14 +33,18 @@ class Log():
     def df(self):
         return self.__df
     
-    def get_iuput_file_hash(self):
+    def get_hash(self):
         return (self.testcase_num, self.file_content_hash, self.file_name_hash)
 
-class LogManager():
+class LogViewer():
     def __init__(self):
+        loader = FileSystemLoader(r"testcase_runner\templates")
+        self.environment = Environment(loader=loader)
+
         def is_log_dir(path: str)->bool:
             path = os.path.join(path, log_file)
             return os.path.isfile(path)
+
         self.logs: List[Log] = []
         for dir in glob.glob(os.path.join(log_dir, "*")):
             if is_log_dir(dir):
@@ -45,22 +52,59 @@ class LogManager():
                 log = Log(path)
                 self.logs.append(log)
         
-    def is_same_inputfiles(self, index_list: List[int]):
+    def is_inputfiles_same(self, index_list: List[int]):
         s = set()
         for index in index_list:
-            s.add(self.logs[index].get_iuput_file_hash())
+            s.add(self.logs[index].get_hash())
         return len(s) == 1
 
     def show_two_data(self, index1: int, index2: int):
-        if not self.is_same_inputfiles(index1, index2):
+        if not self.is_inputfiles_same([index1, index2]):
             return
         merged_df = pd.merge(self.logs[index1].df, self.logs[index2].df, on='input_file')
         print(merged_df)
     
     def get_data_frame(self, index: int):
         return self.logs[index].df
+    
+    def make_html_contents(self):
+        template = self.environment.get_template("log_viewer_main.j2")  # ファイル名を指定
+        data = {
+            "css_list": self.make_css_list(),
+            "table": self.make_table(),
+            }
+        return template.render(data)
 
-log_manager = LogManager()
+    def make_table(self):
+        ret = []
+        checkbox_template = self.environment.get_template("checkbox.j2")
+        cell_template = self.environment.get_template("cell.j2")
+        cell_with_link_template = self.environment.get_template("cell_with_file_link.j2")
+        for i, log in enumerate(self.logs):
+            d = {}
+            checkbox = checkbox_template.render({"name": i})
+            d["check_box"] = cell_template.render({"value": checkbox})
+            data = {
+                "link": log.html_path,
+                "value": log.foleder_name,
+            }
+            d["folder name"] = cell_with_link_template.render(data)
+            if log.has_score:
+                score = log.average_score
+            else:
+                score = "None"
+            d["average score"] = cell_template.render({"value": score})
+            ret.append(d)
+        return ret
+    
+    def make_css_list(self):
+        template = self.environment.get_template("css.j2")
+        ret = [
+            template.render({"link": r"https://newcss.net/new.min.css"}),
+        ]
+        return ret
+
+log_manager = LogViewer()
 
 app = Flask(__name__)
 CORS(app)
@@ -68,7 +112,7 @@ CORS(app)
 @app.route('/api/log_table', methods=['GET'])  # /data エンドポイントに対するGETリクエストを処理
 def get_data():
     # レスポンスの内容を返す
-    return os.getcwd()
+    return log_manager.make_html_contents()
 
 if __name__ == '__main__':
     app.run(debug=True)  # デバッグモードでFlaskアプリケーションを実行
