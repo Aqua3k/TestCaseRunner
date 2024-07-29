03fbc9b72a74ca8545811f3acc7ebcec0d31e434
Aqau3k <c3ryumon@gmail.com>
2024-07-29 18:04:04 +0900

    HTMLの作り方を修正

--- src/testcaserunner/testcase_logger.py
+++ src/testcaserunner/testcase_logger.py
@@ -167,48 +167,50 @@ class RunnerLogManager:
                 hash_obj.update(chunk)
         return hash_obj.hexdigest()
 
-class HtmlParser:
-    logger = RunnerLogger("HtmlParser")
-    def __init__(self, runner_log: RunnerLog, output_path: str, debug: bool) -> None:
-        if debug:
-            self.logger.enable_debug_mode()
-        self.runner_log = runner_log
-        self.output_path = output_path
-        self.base_dir = os.path.split(__file__)[0]
-        loader = FileSystemLoader(os.path.join(self.base_dir, r"templates"))
-        self.environment = Environment(loader=loader)
+class HtmlSection:
+    def __init__(self, environment: Environment, log: RunnerLog):
+        self.environment = environment
+        self.log = log
 
-    status_texts = {
-        ResultStatus.AC: ("AC", "lime"),
-        ResultStatus.WA: ("WA", "gold"),
-        ResultStatus.RE: ("RE", "gold"),
-        ResultStatus.TLE: ("TLE", "gold"),
-        ResultStatus.IE: ("IE", "red"),
-    }
-    
+    def get_html_text(self) -> str:
+        return ""
+
+    def load_file(self, file: str) -> str:
+        with open(file, mode="r", encoding="utf-8") as f:
+            text = f.read()
+        return text
+
+class HtmlFigureSection(HtmlSection):
+    def __init__(self, environment: Environment, log: RunnerLog):
+        super().__init__(environment, log)
+
     histgram_fig_name = 'histgram.png'
     heatmap_fig_name = 'heatmap.png'
-    @logger.function_tracer
-    def make_figure(self) -> str:
-        ret = []
+    def get_html_text(self) -> str:
         template = self.environment.get_template("figure.j2")
-        fig = template.render({"link": os.path.join("fig", self.histgram_fig_name)})
-        ret.append(fig)
-        fig = template.render({"link": os.path.join("fig", self.heatmap_fig_name)})
-        ret.append(fig)
-        return "".join(ret)
+        ret = [
+            template.render({"link": os.path.join("fig", self.histgram_fig_name)}),
+            template.render({"link": os.path.join("fig", self.heatmap_fig_name)}),
+        ]
+        return "\n".join(ret)
 
-    @logger.function_tracer
-    def make_summary(self) -> str:
+class HtmlSummarySection(HtmlSection):
+    def __init__(self, environment: Environment, log: RunnerLog):
+        super().__init__(environment, log)
+
+    def get_html_text(self) -> str:
         template = self.environment.get_template("summary.j2")
         data = {
-            "date" : self.runner_log.metadata["created_date"],
-            "summary": f"<pre>{self.runner_log.df.describe()}</pre>",
+            "date" : self.log.metadata["created_date"],
+            "summary": f"<pre>{self.log.df.describe()}</pre>",
         }
         return template.render(data)
 
-    @logger.function_tracer
-    def make_table(self) -> str:
+class HtmlTableSection(HtmlSection):
+    def __init__(self, environment: Environment, log: RunnerLog):
+        super().__init__(environment, log)
+
+    def get_html_text(self) -> str:
         template = self.environment.get_template("table.j2")
         data = {
             "table": self.make_table_contents(),
@@ -216,63 +218,12 @@ class HtmlParser:
         }
         return template.render(data)
 
-    html_file_name = "result.html"
-    @logger.function_tracer
-    def make_html(self) -> None:
-        template = self.environment.get_template("main.j2")
-        data = {
-            "sections": [
-                *self.make_header_script_list(),
-                *self.make_css_list(),
-                self.make_summary(),
-                self.make_figure(),
-                self.make_table(),
-                *self.make_footer_script_list(),
-            ]
-            }
-        html_file_path = os.path.join(self.output_path, self.html_file_name)
-        with open(html_file_path, mode="w") as f:
-            f.write(template.render(data))
-
-    @logger.function_tracer    
-    def get_url_cell(self, column: str, row: int) -> str:
-        value = self.runner_log._df_at(column, row)
-        template = self.environment.get_template("cell_with_file_link.j2")
-        data = {
-            "link": value,
-            "value": "+",
-            }
-        return template.render(data)
-    
-    @logger.function_tracer
-    def get_status_cell(self, column: str, row: int) -> str:
-        value = self.runner_log._df_at(column, row)
-        text, color = self.status_texts.get(value, ("IE", "red"))
-        template = self.environment.get_template("cell_with_color.j2")
-        data = {
-            "color": color,
-            "value": text,
-            }
-        return template.render(data)
-    
-    @logger.function_tracer
-    def get_text_cell(self, column: str, row: int) -> str:
-        value = self.runner_log._df_at(column, row)
-        if type(value) is np.float64 or type(value) is np.float32:
-            value = round(value, 3)
-        template = self.environment.get_template("cell.j2")
-        data = {
-            "value": value,
-            }
-        return template.render(data)
-
-    @logger.function_tracer
     def make_table_contents(self) -> list[dict[str, str]]:
         ret = []
-        for row in range(self.runner_log.metadata["testcase_num"]):
+        for row in range(self.log.metadata["testcase_num"]):
             rows = {}
-            for column in self.runner_log.df.columns:
-                match self.runner_log.metadata["attributes"][column]:
+            for column in self.log.df.columns:
+                match self.log.metadata["attributes"][column]:
                     case HtmlColumnType.URL:
                         value = self.get_url_cell(column, row)
                     case HtmlColumnType.STATUS:
@@ -286,36 +237,11 @@ class HtmlParser:
                 rows[column] = value
             ret.append(rows)
         return ret
-    
-    @logger.function_tracer
-    def load_file(self, file: str) -> str:
-        with open(file, mode="r", encoding="utf-8") as f:
-            text = f.read()
-        return text
-    
-    @logger.function_tracer
-    def make_header_script_list(self) -> list[str]:
-        template = self.environment.get_template("script.j2")
-        file = os.path.join(os.path.split(__file__)[0], "js/Table.js")
-        ret = [
-            template.render({"text": self.load_file(file)}),
-        ]
-        return ret
 
-    @logger.function_tracer
-    def make_footer_script_list(self) -> list[str]:
-        template = self.environment.get_template("script.j2")
-        file = os.path.join(os.path.split(__file__)[0], "js/checkbox.js")
-        ret = [
-            template.render({"text": self.load_file(file)}),
-        ]
-        return ret
-    
-    @logger.function_tracer
     def make_table_columns(self) -> dict[str, str]:
         table_columns = dict()
-        for column in self.runner_log.df.columns:
-            match self.runner_log.metadata["attributes"][column]:
+        for column in self.log.df.columns:
+            match self.log.metadata["attributes"][column]:
                 case HtmlColumnType.URL|HtmlColumnType.STATUS:
                     table_columns[column] = "normal"
                 case HtmlColumnType.TEXT:
@@ -326,8 +252,65 @@ class HtmlParser:
                     assert "error: 不明なHtmlColumnTypeがあります。"
         return table_columns
 
-    @logger.function_tracer
-    def make_css_list(self) -> list[str]:
+    def get_url_cell(self, column: str, row: int) -> str:
+        value = self.log._df_at(column, row)
+        template = self.environment.get_template("cell_with_file_link.j2")
+        data = {
+            "link": value,
+            "value": "+",
+            }
+        return template.render(data)
+    
+    status_texts = {
+        ResultStatus.AC: ("AC", "lime"),
+        ResultStatus.WA: ("WA", "gold"),
+        ResultStatus.RE: ("RE", "gold"),
+        ResultStatus.TLE: ("TLE", "gold"),
+        ResultStatus.IE: ("IE", "red"),
+    }
+    def get_status_cell(self, column: str, row: int) -> str:
+        value = self.log._df_at(column, row)
+        text, color = self.status_texts.get(value, ("IE", "red"))
+        template = self.environment.get_template("cell_with_color.j2")
+        data = {
+            "color": color,
+            "value": text,
+            }
+        return template.render(data)
+    
+    def get_text_cell(self, column: str, row: int) -> str:
+        value = self.log._df_at(column, row)
+        if type(value) is np.float64 or type(value) is np.float32:
+            value = round(value, 3)
+        template = self.environment.get_template("cell.j2")
+        data = {
+            "value": value,
+            }
+        return template.render(data)
+
+class HtmlHeaderScriptSection(HtmlSection):
+    def __init__(self, environment: Environment, log: RunnerLog):
+        super().__init__(environment, log)
+
+    def get_html_text(self) -> str:
+        template = self.environment.get_template("script.j2")
+        file = os.path.join(os.path.split(__file__)[0], "js/Table.js")
+        return template.render({"text": self.load_file(file)})
+
+class HtmlFooterScriptSection(HtmlSection):
+    def __init__(self, environment: Environment, log: RunnerLog):
+        super().__init__(environment, log)
+
+    def get_html_text(self) -> str:
+        template = self.environment.get_template("script.j2")
+        file = os.path.join(os.path.split(__file__)[0], "js/checkbox.js")
+        return template.render({"text": self.load_file(file)})
+
+class HtmlCssSection(HtmlSection):
+    def __init__(self, environment: Environment, log: RunnerLog):
+        super().__init__(environment, log)
+
+    def get_html_text(self) -> str:
         template1 = self.environment.get_template("css_link.j2")
         template2 = self.environment.get_template("css.j2")
         file = os.path.join(os.path.split(__file__)[0], "js/SortTable.css")
@@ -335,4 +318,37 @@ class HtmlParser:
             template2.render({"text": self.load_file(file)}),
             template1.render({"link": r"https://newcss.net/new.min.css"}),
         ]
-        return ret
+        return "".join(ret)
+
+class HtmlParser:
+    logger = RunnerLogger("HtmlParser")
+    def __init__(self, runner_log: RunnerLog, output_path: str, debug: bool) -> None:
+        if debug:
+            self.logger.enable_debug_mode()
+        self.runner_log = runner_log
+        self.output_path = output_path
+        self.base_dir = os.path.split(__file__)[0]
+        self.sections: list[type[HtmlSection]] = [
+            HtmlHeaderScriptSection,
+            HtmlCssSection,
+            HtmlSummarySection,
+            HtmlFigureSection,
+            HtmlTableSection,
+            HtmlFooterScriptSection,
+        ]
+    html_file_name = "result.html"
+    @logger.function_tracer
+    def make_html(self) -> None:
+        loader = FileSystemLoader(os.path.join(self.base_dir, r"templates"))
+        environment = Environment(loader=loader)
+        template = environment.get_template("main.j2")
+        texts: list[str] = []
+        for section in self.sections:
+            sect = section(environment, self.runner_log)
+            text = sect.get_html_text()
+            print(text)
+            texts.append(sect.get_html_text())
+        data = {"sections": texts}
+        html_file_path = os.path.join(self.output_path, self.html_file_name)
+        with open(html_file_path, mode="w") as f:
+            f.write(template.render(data))
