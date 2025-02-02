427ff9e691b9f81b5a25836be45f43a0c1a3f5e2
Aqau3k <c3ryumon@gmail.com>
2025-02-02 17:24:12 +0900

    テストコードを整備

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -16,6 +16,7 @@ from testcaserunner import (
     NoTestcaseFileException,
     )
 
+# テストコードで使う関数
 def no_error_program(testcase: TestCase):
     cmd = f"python main.py < {testcase.input_file_path}"
     proc = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
@@ -48,6 +49,7 @@ def no_error_program_attribute(testcase: TestCase):
         attrbute["odd"] = int(case)
     return TestCaseResult(attribute=attrbute)
 
+# フィクスチャ
 @pytest.fixture
 def setup_normally():
     """logフォルダを消してno_filesフォルダを作る"""
@@ -57,102 +59,96 @@ def setup_normally():
         shutil.rmtree("no_files")
     os.mkdir("no_files")
 
-# 警告も例外も出ない
-def test_no_error_no_warning_case0(caplog, setup_normally):
+# テストコード
+def test_arguments_are_valid1(caplog, setup_normally):
+    # 警告なしで正常に処理が終わる
     with caplog.at_level(logging.WARNING):
         run(testcase_handler=no_error_program, input_file_path="in")
     assert len(caplog.records) == 0
 
-def test_no_error_no_warning_case1(caplog, setup_normally):
-    with caplog.at_level(logging.WARNING):
-        run(testcase_handler=no_error_program, input_file_path="in", copy_target_files=["main.py"])
-    assert len(caplog.records) == 0
-
-def test_no_error_no_warning_case4(caplog, setup_normally):
-    with caplog.at_level(logging.WARNING):
-        run(testcase_handler=no_error_program, input_file_path="in")
-    assert len(caplog.records) == 0
-
-def test_no_error_no_warning_case5(caplog, setup_normally):
-    with caplog.at_level(logging.WARNING):
-        run(testcase_handler=no_error_program, input_file_path="in", parallel_processing_method="process")
-    assert len(caplog.records) == 0
-
-def test_no_error_no_warning_case6(caplog, setup_normally):
-    with caplog.at_level(logging.WARNING):
-        run(testcase_handler=no_error_program, input_file_path="in", parallel_processing_method="PROCESS")
-    assert len(caplog.records) == 0
-
-def test_no_error_no_warning_case7(caplog, setup_normally):
-    with caplog.at_level(logging.WARNING):
-        run(testcase_handler=no_error_program, input_file_path="in", parallel_processing_method="thread")
-    assert len(caplog.records) == 0
-
-def test_no_error_no_warning_case8(caplog, setup_normally):
-    with caplog.at_level(logging.WARNING):
-        run(testcase_handler=no_error_program, input_file_path="in", parallel_processing_method="THREAD")
-    assert len(caplog.records) == 0
-
-def test_no_error_no_warning_case9(caplog, setup_normally):
+def test_arguments_are_valid2(caplog, setup_normally):
+    # 登録したハンドラの中でエラーがあっても正常終了する
     run(testcase_handler=error_program, input_file_path="in")
 
-def test_no_error_no_warning_case10(caplog, setup_normally):
-    with caplog.at_level(logging.WARNING):
-        run(testcase_handler=no_error_program, input_file_path="in", parallel_processing_method="single")
-    assert len(caplog.records) == 0
-
-def test_no_error_no_warning_case11(caplog, setup_normally):
-    with caplog.at_level(logging.WARNING):
-        run(testcase_handler=no_error_program, input_file_path="in", parallel_processing_method="SINGLE")
-    assert len(caplog.records) == 0
-
-def test_no_error_no_warning_case12(caplog, setup_normally):
+def test_arguments_are_valid3(caplog, setup_normally):
+    # attributeへ情報を登録することができる
     with caplog.at_level(logging.WARNING):
         run(testcase_handler=no_error_program_attribute, input_file_path="in")
     assert len(caplog.records) == 0
 
 def test_no_error_no_warning_case13(caplog, setup_normally):
+    # すべての引数に値を設定する
     with caplog.at_level(logging.WARNING):
         run(testcase_handler=no_error_program, input_file_path="in",\
              parallel_processing_method="single", repeat_count=2)
     assert len(caplog.records) == 0
 
-# 例外が出る
-def test_with_error_case0(setup_normally):
+def test_2nd_argument_is_invalid1(setup_normally):
+    # 第二引数へ存在しないパスを入れたらInvalidPathException
     with pytest.raises(InvalidPathException):
         run(testcase_handler=no_error_program, input_file_path="foo")
 
-def test_with_error_case1(setup_normally):
+def test_2nd_argument_is_invalid2(setup_normally):
+    # 第二引数で指定されたフォルダでファイルがなければNoTestcaseFileException
     with pytest.raises(NoTestcaseFileException):
         run(testcase_handler=no_error_program, input_file_path="no_files")
 
-def test_with_error_case2(setup_normally):
+def test_3rd_argument_is_invalid(setup_normally):
+    # 第三引数で負の数を指定したらエラー
     with pytest.raises(ValueError):
         run(testcase_handler=no_error_program, input_file_path="no_files", repeat_count=-1)
 
-def test_with_error_case3(setup_normally):
-    with pytest.raises(ValueError):
-        run(testcase_handler=no_error_program, input_file_path="in", parallel_processing_method="test")
+def test_4th_arguments_is_valid(caplog, setup_normally):
+    with caplog.at_level(logging.WARNING):
+        run(testcase_handler=no_error_program, input_file_path="in", copy_target_files=["main.py"])
+    assert len(caplog.records) == 0
 
-# 警告が出る
-def test_with_warning_case0(caplog, setup_normally):
+def test_4th_argument_is_invalid1(caplog, setup_normally):
     with caplog.at_level(logging.WARNING):
         run(testcase_handler=no_error_program, input_file_path="in", copy_target_files=["not_exist"])
     assert len(caplog.records) == 1
 
-def test_with_warning_case1(caplog, setup_normally):
+def test_4th_argument_is_invalid2(caplog, setup_normally):
     with caplog.at_level(logging.WARNING):
         run(testcase_handler=no_error_program, input_file_path="in", copy_target_files=["in"])
     assert len(caplog.records) == 1
 
-def test_with_warning_case2(caplog, setup_normally):
+def test_5th_argument_is_valid1(caplog, setup_normally):
     with caplog.at_level(logging.WARNING):
-        run(testcase_handler=error_program, input_file_path="in", parallel_processing_method="single")
-    assert len(caplog.records) != 0
+        run(testcase_handler=no_error_program, input_file_path="in", parallel_processing_method="process")
+    assert len(caplog.records) == 0
 
-# デバッグメッセージのテスト
-def test_with_debugmessage_case0(caplog, setup_normally):
-    # デバッグ起動でデバッグメッセージが出る
+def test_5th_argument_is_valid2(caplog, setup_normally):
+    with caplog.at_level(logging.WARNING):
+        run(testcase_handler=no_error_program, input_file_path="in", parallel_processing_method="PROCESS")
+    assert len(caplog.records) == 0
+
+def test_5th_argument_is_valid3(caplog, setup_normally):
+    with caplog.at_level(logging.WARNING):
+        run(testcase_handler=no_error_program, input_file_path="in", parallel_processing_method="thread")
+    assert len(caplog.records) == 0
+
+def test_5th_argument_is_valid4(caplog, setup_normally):
+    with caplog.at_level(logging.WARNING):
+        run(testcase_handler=no_error_program, input_file_path="in", parallel_processing_method="THREAD")
+    assert len(caplog.records) == 0
+
+def test_5th_argument_is_valid5(caplog, setup_normally):
+    with caplog.at_level(logging.WARNING):
+        run(testcase_handler=no_error_program, input_file_path="in", parallel_processing_method="single")
+    assert len(caplog.records) == 0
+
+def test_5th_argument_is_valid6(caplog, setup_normally):
+    with caplog.at_level(logging.WARNING):
+        run(testcase_handler=no_error_program, input_file_path="in", parallel_processing_method="SINGLE")
+    assert len(caplog.records) == 0
+
+def test_5th_argument_is_invalid(setup_normally):
+    with pytest.raises(ValueError):
+        run(testcase_handler=no_error_program, input_file_path="in", parallel_processing_method="test")
+
+def test_debug_message(caplog, setup_normally):
+    # デバッグメッセージが出力される
     with caplog.at_level(logging.DEBUG):
         run(testcase_handler=no_error_program, input_file_path="in", _debug=True)
     assert len(caplog.records) != 0
