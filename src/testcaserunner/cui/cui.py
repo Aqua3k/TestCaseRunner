9b99e5ea42ea9063e89134e7d038ca9c23b9b850
Aqau3k <c3ryumon@gmail.com>
2025-03-13 23:51:22 +0900

    修正

--- src/testcaserunner/cui/cui.py
+++ src/testcaserunner/cui/cui.py
@@ -1,130 +1,8 @@
 from rich import print
 import msvcrt
-from enum import Enum, auto
-
-from rich.console import Console
-from rich.live import Live
-from rich.table import Table
-from rich.panel import Panel
-from rich.columns import Columns
 
 from ..debug import Logger
-from ..defines import InternalError
-from .database import Database
-
-class KeyboardInputs(Enum):
-    UP = auto()
-    DOWN = auto()
-    LEFT = auto()
-    RIGHT = auto()
-    QUIT = auto()
-    ENTER = auto()
-    OTHER = auto()
-
-class ScreenTableBase:
-    def __init__(self, title: str = "") -> None:
-        self.table = Table(title=title)
-
-    def clear(self) -> None:
-        self.table.columns.clear()
-        self.table.rows.clear()
-
-    def add_row(self, columns: list[str]) -> None:
-        self.table.add_row(*columns)
-
-    def add_column(self, name: str, **kwargs) -> None:
-        self.table.add_column(name, **kwargs)
-
-    def get_table(self) -> Table:
-        return self.table
-
-class MainMenuTable(ScreenTableBase):
-    def __init__(self) -> None:
-        super().__init__(title="Menu")
-
-    options = [
-        "1. View All Results",
-        "2. Sort Results",
-        "3. Delete Result",
-        "4. Compare Results",
-        "5. Exit",
-    ]
-    def update(self):
-
-        selected = 0 # TODO: 選択された項目を保持する変数を追加する
-
-        # テーブルの列をクリア
-        self.clear()
-
-        self.add_column("Select", justify="center")
-        self.add_column("Menu")
-
-        for i, option in enumerate(self.options):
-            marker = ">" if i == selected else ""
-            self.add_row([marker, option])
-
-class ResultTable(ScreenTableBase):
-    def __init__(self) -> None:
-        super().__init__(title="Results")
-
-    def update(self):
-        database = Database()
-
-        # テーブルの列をクリア
-        self.clear()
-
-        self.add_column("ID")
-        self.add_column("Created Date")
-        self.add_column("Link")
-
-        for attribute in database.get_attributes():
-            self.add_column(attribute)
-
-        for i, log in enumerate(database.iterate_logs()):
-            columns = []
-
-            columns.append(f"{i+1}")
-            metadata = log.get_metadata()
-            columns.append(metadata.created_date)
-            columns.append(f"[link={log.get_html_path()}]+[/link]")
-
-            for attribute in database.get_attributes():
-                data  = log.get(attribute)
-                if isinstance(data, float):
-                    columns.append(f"{data:.2f}")  # 小数点以下2桁にフォーマット
-                else:
-                    columns.append(str(data))
-            self.add_row(columns)
-
-class Screen:
-    def __init__(self) -> None:
-        self.result_table = ResultTable()
-        self.menu_table = MainMenuTable()
-        self.console = Console()
-        self.live = None
-
-    def __enter__(self):
-        self.live = Live(console=self.console, refresh_per_second=10, screen=True)
-        self.live.__enter__()
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.live.__exit__(exc_type, exc_value, traceback)
-
-    def display(self) -> None:
-        # テーブルを更新
-        self.result_table.update()
-        self.menu_table.update()
-
-        # パネルにテーブルを追加
-        result_panel = Panel(self.result_table.get_table(), title="Results")
-        menu_panel = Panel(self.menu_table.get_table(), title="Menu")
-
-        # カラムにパネルを追加
-        columns = Columns([result_panel, menu_panel])
-
-        # Liveを更新
-        self.live.update(columns)
+from .screen import KeyboardInputs, Screen
 
 class CUI:
     def __init__(self):
@@ -152,9 +30,9 @@ class CUI:
     def main_loop(self) -> None:
         with Screen() as screen:
             while True:
-                screen.display()
+                screen.update()
                 key = self.handle_keybord_input()
-                if key == KeyboardInputs.QUIT or key == KeyboardInputs.OTHER:
+                if screen.notify_keyboard_input(key):
                     break
 
     def activate(self) -> None:
--- src/testcaserunner/cui/screen.py
+++ src/testcaserunner/cui/screen.py
@@ -1,62 +1,85 @@
-import os
 from enum import Enum, auto
 from abc import ABC, abstractmethod
 
-from rich import print
-from rich.table import Table
 from rich.console import Console
+from rich.live import Live
+from rich.table import Table
+from rich.panel import Panel
+from rich.columns import Columns
 
-from ..debug import call_logger
 from .database import Database
 
-class ScreenStatus(Enum):
-    MAIN_SCREEN = auto()
-    VIEW_RESULTS = auto()
-    SORT_RESULTS = auto()
-    DELETE_RESULTS = auto()
-    COMPARE_RESULTS = auto()
+class KeyboardInputs(Enum):
+    UP = auto()
+    DOWN = auto()
+    LEFT = auto()
+    RIGHT = auto()
+    QUIT = auto()
+    ENTER = auto()
+    OTHER = auto()
 
-class BaseScreen(ABC):
-    @call_logger
-    def open(self) -> None:
-        pass
-    
-    @call_logger
-    def close(self) -> None:
-        pass
+class ScreenTableBase(ABC):
+    def __init__(self, title: str = "") -> None:
+        self.table = Table(title=title)
+
+    def clear(self) -> None:
+        self.table.columns.clear()
+        self.table.rows.clear()
+
+    def add_row(self, columns: list[str]) -> None:
+        self.table.add_row(*columns)
+
+    def add_column(self, name: str, **kwargs) -> None:
+        self.table.add_column(name, **kwargs)
+
+    def get_table(self) -> Table:
+        return self.table
 
     @abstractmethod
-    def display(self) -> None:
+    def update(self) -> None:
         pass
 
-    @abstractmethod
-    def handle_input(self, key: str) -> ScreenStatus:
-        pass
+class MainMenuTable(ScreenTableBase):
+    def __init__(self) -> None:
+        super().__init__(title="Menu")
 
-class MainScreen(BaseScreen):
-    main_menu_string = (
-        "=== Test Result Manager ===\n"
-        "1. View All Results\n"
-        "2. Sort Results\n"
-        "3. Delete Result\n"
-        "4. Compare Results\n"
-        "5. Exit"
-        )
-    def display(self) -> None:
-        print(f"current directory: {os.getcwd()}")
+    options = [
+        "1. View All Results",
+        "2. Sort Results",
+        "3. Delete Result",
+        "4. Compare Results",
+        "5. Exit",
+    ]
 
-        print(self.main_menu_string)
+    def update(self):
+        selected = 0  # TODO: 選択された項目を保持する変数を追加する
+
+        # テーブルの列をクリア
+        self.clear()
+
+        self.add_column("Select", justify="center")
+        self.add_column("Menu")
+
+        for i, option in enumerate(self.options):
+            marker = ">" if i == selected else ""
+            self.add_row([marker, option])
+
+class ResultTable(ScreenTableBase):
+    def __init__(self) -> None:
+        super().__init__(title="Results")
+
+    def update(self):
         database = Database()
 
-        # テーブル作成
-        table = Table(title="Test Results")
+        # テーブルの列をクリア
+        self.clear()
 
-        table.add_column("ID")
-        table.add_column("Created Date")
-        table.add_column("Link")
+        self.add_column("ID")
+        self.add_column("Created Date")
+        self.add_column("Link")
 
         for attribute in database.get_attributes():
-            table.add_column(attribute)
+            self.add_column(attribute)
 
         for i, log in enumerate(database.iterate_logs()):
             columns = []
@@ -72,11 +95,47 @@ class MainScreen(BaseScreen):
                     columns.append(f"{data:.2f}")  # 小数点以下2桁にフォーマット
                 else:
                     columns.append(str(data))
-            table.add_row(*columns)
+            self.add_row(columns)
 
-        # テーブルを表示
-        console = Console()
-        console.print(table)
+class Screen:
+    def __init__(self) -> None:
+        self.result_table = ResultTable()
+        self.menu_table = MainMenuTable()
+        self.console = Console()
+        self.live = None
+
+    def __enter__(self):
+        self.live = Live(console=self.console, refresh_per_second=10, screen=True)
+        self.live.__enter__()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.live.__exit__(exc_type, exc_value, traceback)
     
-    def handle_input(self, key: str) -> ScreenStatus:
-        return ScreenStatus.MAIN_SCREEN
+    def notify_keyboard_input(self, key: KeyboardInputs) -> bool:
+        """キーボード入力を通知する
+
+        Args:
+            key (KeyboardInputs): キーボード入力
+
+        Returns:
+            bool: True: 終了, False: 継続
+        """
+        if key == KeyboardInputs.QUIT or key == KeyboardInputs.OTHER:
+            return True
+        return False
+
+    def update(self) -> None:
+        # テーブルを更新
+        self.result_table.update()
+        self.menu_table.update()
+
+        # パネルにテーブルを追加
+        result_panel = Panel(self.result_table.get_table(), title="Results")
+        menu_panel = Panel(self.menu_table.get_table(), title="Menu")
+
+        # カラムにパネルを追加
+        columns = Columns([result_panel, menu_panel])
+
+        # Liveを更新
+        self.live.update(columns)
